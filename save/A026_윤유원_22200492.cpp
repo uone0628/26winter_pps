046094cf5757e92cefad988899ef5396e8e6acf3
uone0628 <yu.want.coding@gmail.com>
2026-01-15 02:38:27 +0900

    week2 day1

---
+++ save/A026_윤유원_22200492.cpp
@@ -0,0 +1,17 @@
+#include <string>
+#include <vector>
+using namespace std;
+
+bool solution(int x) {
+    bool answer = true;
+    int sum=0;
+    int num=x;
+    while(num>0)
+    {
+        sum+=num%10;
+        num/=10;
+    }
+    if(x%sum!=0)
+        answer=false;
+    return answer;
+}
