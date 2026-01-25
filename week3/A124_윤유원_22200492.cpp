17de61b8e709e46e2f109847c97a15c11f1d27c2
uone0628 <yu.want.coding@gmail.com>
2026-01-26 03:34:29 +0900

    week3 day4

---
+++ week3/A124_윤유원_22200492.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+    while (n > 1) 
+        for (int i = 2; i <= n; i++)
+            if (n % i == 0) {
+                cout << i << endl;
+                n /= i;
+                break;
+            }
+    return 0;
+}
